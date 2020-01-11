cc05bd682ac9e7cb7c9c7d7b5fbb53ba65d34beb
Code X <code@codex.net.au>
2020-01-12 01:06:10 +1100

    Update to make more readable
    Reading grade 10

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,16 +7,15 @@
     <p class="font-smaller">
       In a natural disaster emergency, we help you find the help you need, or find where and how you can help.
     </p>
-    <!-- Comment this div out before launch-->
+    
     <div class="alert alert-primary w-100" role="alert">
         This website is still under active development, go-live should only be a few hours away &lt;3
     </div>
     
-    <!-- Uncomment this div before launch
     <div style="text-align:center" class="alert alert-primary w-100" role="alert">
-    Disclaimer: While every effort is being made to ensure the information is accurate, this is a community-sourced directory moderated by volunteers. Information may be incomplete, inaccurate, or out-of-date. Please verify.
+     Disclaimer. This is a community-sourced directory moderated by volunteers.  We do our best to make sure information is accurate.  Information may change or become out-of-date. Please verify.
     </div>
-    -->
+    
 </nav>
 </template>
 
