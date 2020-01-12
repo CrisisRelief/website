9db8bcf7109a89111617ceae128e057262cd8fb6
Derwent <derwent.m@thoughtworks.com>
2020-01-12 11:09:05 +1100

    [CAD-56] 🔧fix dev server only works http

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     devServer: {
         historyApiFallback: false,
-        https: true
     },
     chainWebpack: config => {
         config.module.rule('eslint').use('eslint-loader')
