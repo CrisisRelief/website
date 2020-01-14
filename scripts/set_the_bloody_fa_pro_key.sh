25220957a38b4c1bdad87d34f79407fbae9c28b3
Derwent <derwent.m@thoughtworks.com>
2020-01-14 17:15:35 +1100

    [CAD-92] 🐛 make sed unix compatible

--- scripts/set_the_bloody_fa_pro_key.sh
+++ scripts/set_the_bloody_fa_pro_key.sh
@@ -4,4 +4,6 @@ if test -z "$FONTAWESOME_NPM_AUTH_TOKEN"; then
     echo "FONTAWESOME_NPM_AUTH_TOKEN not set"
     exit 1
 fi
-sed -i "s/!!!FA PRO KEY HERE!!!/${FONTAWESOME_NPM_AUTH_TOKEN}/g" .npmrc
+INPLACE=".npmrc"
+sed "s/!!!FA PRO KEY HERE!!!/${FONTAWESOME_NPM_AUTH_TOKEN}/g" > ${INPLACE}.tmp < ${INPLACE} \
+&& mv ${INPLACE}.tmp ${INPLACE}
