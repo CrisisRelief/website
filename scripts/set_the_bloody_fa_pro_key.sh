c24b65b22c1386aef15cc08ed2c4509b56677d6f
gerard <gerardh@thoughtworks.com>
2020-01-14 14:29:21 +1100

    CAD-92 | modify the sed command in the scripts folder for assigning FA environment variable | Gerard

--- scripts/set_the_bloody_fa_pro_key.sh
+++ scripts/set_the_bloody_fa_pro_key.sh
@@ -4,4 +4,4 @@ if test -z "$FONTAWESOME_NPM_AUTH_TOKEN"; then
     echo "FONTAWESOME_NPM_AUTH_TOKEN not set"
     exit 1
 fi
-sed -i "s/!!!FA PRO KEY HERE!!!/${FONTAWESOME_NPM_AUTH_TOKEN}/g" .npmrc
+sed -i '' "s/!!!FA PRO KEY HERE!!!/${FONTAWESOME_NPM_AUTH_TOKEN}/g" .npmrc
